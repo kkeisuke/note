acb7949d28beac83711fc9e387f4111a7b38f51d
kimura keisuke <mail@kkeisuke.com>
2020-09-03 13:51:42 +0900

    eslint を「強く推奨」に修正

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,41 @@
 module.exports = {
   root: true,
+
   env: {
     node: true
   },
+
   extends: [
-    'plugin:vue/vue3-essential',
+    'plugin:vue/strongly-recommended',
     'eslint:recommended',
     '@vue/typescript/recommended',
     '@vue/prettier',
     '@vue/prettier/@typescript-eslint'
   ],
+
   parserOptions: {
     ecmaVersion: 2020
   },
+
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'vue/array-bracket-spacing': 'error',
+    'vue/arrow-spacing': 'error',
+    'vue/block-spacing': 'error',
+    'vue/brace-style': 'error',
+    'vue/camelcase': 'error',
+    'vue/component-name-in-template-casing': 'error',
+    'vue/eqeqeq': 'error',
+    'vue/key-spacing': 'error',
+    'vue/match-component-file-name': 'error',
+    'vue/object-curly-spacing': 'error',
+    'vue/script-indent': 'error',
+    'vue/space-infix-ops': 'error',
+    'vue/space-unary-ops': 'error',
+    'vue/v-on-function-call': 'error',
+    'no-console': 'warn',
+    'no-debugger': 'warn'
   },
+
   overrides: [
     {
       files: [
