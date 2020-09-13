c70835f94298b0f6cd9050e36732015d76a3860b
kimura keisuke <mail@kkeisuke.com>
2020-09-13 15:47:35 +0900

    ヘッダー追加

---
+++ src/components/Layout/NoteHeader.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1 class="NoteHeader bg-gray-700 p-2">
+    <span class="text-xl text-white font-semibold">{{ title }}</span>
+  </h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'NoteHeader',
+  props: {
+    title: {
+      type: String,
+      default: 'Note'
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped></style>
