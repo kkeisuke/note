1157df1e242a82bc3639d1fea29afea28e49f55c
kimura keisuke <mail@kkeisuke.com>
2020-09-18 16:54:09 +0900

    ヘッダーにメニュー追加

--- src/components/Layout/NoteHeader.vue
+++ src/components/Layout/NoteHeader.vue
@@ -1,14 +1,21 @@
 <template>
   <h1 class="NoteHeader bg-gray-700 p-2">
-    <span class="text-xl text-white font-semibold">{{ title }}</span>
+    <div class="flex justify-between items-center">
+      <span class="text-xl text-white font-semibold">{{ title }}</span>
+      <NoteHeaderSubMenu></NoteHeaderSubMenu>
+    </div>
   </h1>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import NoteHeaderSubMenu from '@/components/Layout/NoteHeaderSubMenu.vue'
 
 export default defineComponent({
   name: 'NoteHeader',
+  components: {
+    NoteHeaderSubMenu
+  },
   props: {
     title: {
       type: String,
---
+++ src/components/Layout/NoteHeaderSubMenu.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="NoteHeaderSubMenu flex items-center">
+    <a href="https://kkeisuke.com/" target="_blank">
+      <svg class="inline-block h-4 w-4 text-white">
+        <use :xlink:href="house" />
+      </svg>
+    </a>
+  </div>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'NoteHeaderSubMenu',
+  setup() {
+    return {
+      house: computed(() => `${require('bootstrap-icons/bootstrap-icons.svg')}#house-fill`)
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped></style>
