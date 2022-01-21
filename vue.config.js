c02dc35a21650e5c2d4ab9f85b299325e41be810
kimura keisuke <mail@kkeisuke.com>
2022-01-22 02:19:08 +0900

    tailwindcss, @toast-ui/editor の CSS build 順調整

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,8 @@
 const config = {
   productionSourceMap: false,
   css: {
+    // main.ts で import した順に style が当たるようにするため。
+    // build 時に css ファイルを作らない（順番が変わる。tailwind が最後になってしまう。）
     extract: false
   },
   pwa: {
