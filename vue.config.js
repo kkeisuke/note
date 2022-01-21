10141805ee651cff276ed205d9a7ccaafadf15df
kimura keisuke <mail@kkeisuke.com>
2022-01-22 02:04:50 +0900

    tailwindcss, @toast-ui/editor の CSS build 順調査

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,9 @@
 /** @type {import('@vue/cli-service').ProjectOptions} */
 const config = {
   productionSourceMap: false,
-
+  css: {
+    extract: false
+  },
   pwa: {
     name: 'Note',
     themeColor: '#4A4A4A'
