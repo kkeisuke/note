f6871e6624a71df057841fc3966c43f1488e42ae
kimura keisuke <mail@kkeisuke.com>
2022-03-18 17:02:31 +0900

    設定の更新

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 /** @type {import('@jest/types').Config.InitialOptions} */
 const config = {
-  preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel'
+  preset: '@vue/cli-plugin-unit-jest/presets/typescript'
 }
 
 module.exports = config
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
-/** @type {import('@vue/cli-service').ProjectOptions} */
-const config = {
+const { defineConfig } = require('@vue/cli-service')
+
+const config = defineConfig({
   productionSourceMap: false,
   css: {
     // main.ts で import した順に style が当たるようにするため。
@@ -10,6 +11,6 @@ const config = {
     name: 'Note',
     themeColor: '#4A4A4A'
   }
-}
+})
 
 module.exports = config
